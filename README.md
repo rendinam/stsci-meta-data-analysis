e33e9d10c949fd6ffff8f334c864d872a3441504
Matt Rendina <mrendina@stsci.edu>
2020-12-30 16:59:12 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # stsci-meta-data-analysis
+
+A barebones python package that serves only to pull in top-level dependencies for a selected collection of STScI-developed Python data analysis tools.
---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='stsci-meta-data-analysis',
+    version='0.0.1',
+    install_requires=[
+        'asdf',
+        'astroimtools',
+        'ginga',
+        'gwcs',
+        'photutils',
+        'imexam',
+        'specutils',
+        'spherical-geometry',
+        'stginga',
+        'tweakwcs',
+        'webbpsf'
+    ],
+)
